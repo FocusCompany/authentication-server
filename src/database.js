c764c614f3991f7cc087d33f2b470bc884e18c31
aguado_e <enzo.aguado@epitech.eu>
2017-12-09 21:59:52 +0800

    bootstraping of MySQL connector

---
+++ src/database.js
@@ -0,0 +1,11 @@
+import mysql from 'mysql';
+
+const db_session = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'focus42sh',
+    database : 'authentication_server'
+});
+db_session.connect();
+
+export default db_session;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-const express = require('express');
+import db_session from "./database";
+import express from "express";
+
 const app = express();
 app.listen(3000);
 
-
 app.get('/', (req, res) => res.send("Hello World"));
-
 app.post('/signup', (req, res) => {
     console.log(req);
     res.send('Hitting /signup');
 });
+
+
+
+db_session.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
+db_session.end();

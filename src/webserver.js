b079ebc079a629d15b679f3824e9da1ab5161712
Etienne Pasteur <etienne.pasteur@epitech.eu>
2018-04-10 18:33:28 +0800

    Fix CORS for cross-domain request

--- src/webserver.js
+++ src/webserver.js
@@ -50,6 +50,7 @@ app.use(bodyParser.json());
 // CORS for cross-domain request
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });

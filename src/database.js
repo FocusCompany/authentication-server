87a0733da23b91fa0287ea3b419bca6bdfd8d95d
Enzo AGUADO <enzo.aguado@epitech.eu>
2018-04-24 16:13:07 +0800

    fix connection lost error

--- src/database.js
+++ src/database.js
@@ -1,13 +1,11 @@
 import mysql from 'mysql';
 
-const db_session = mysql.createConnection({
+const db_session = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
     password : 'focus42sh',
     database : 'authentication_server'
 });
-
-db_session.connect();
 
 export default db_session;
 
